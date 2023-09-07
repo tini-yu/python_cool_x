eae2a373e6b94cd059d69421b3a26e69f8871da4
Iaroslava Novoselova <i.novoselova.sas@gmail.com>
2023-09-07 11:20:23 +0300

    lecture 3 update

--- practice_3/simple_guess.py
+++ practice_3/simple_guess.py
@@ -7,12 +7,10 @@ n = int(input("N:"))
 
 def guess(num: int)-> int | str:
     counter = 0
-    for i in range(1, (num//2)+1): # обрезаем половину, потому что после половины, квадрат всегда будет больше n
+    for i in range(1, (num//2)+2): # обрезаем, потому что после половины, квадрат всегда будет больше n
         counter += 1
         if i*i == num:
             return i
-        else:
-            if counter == num//2:
-                return "Трудно, не могу."
+    return "Трудно, не могу."
 
 print(guess(n))
